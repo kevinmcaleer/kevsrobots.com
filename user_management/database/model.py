70802e8b527ebf930b9f17b87d302891b2320ddd
Kevin McAleer <kevinmcaleer@gmail.com>
2024-01-03 15:49:19 +0000

    Added Cubie and slam

--- user_management/database/model.py
+++ user_management/database/model.py
@@ -1,12 +1,15 @@
-from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, PositiveInt
 from typing import Optional
 
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     profile_picture: Optional[bytes] = None
-
-class UserDisplay(BaseModel):
-    id: int
+class User(BaseModel):
+    id: int  
+    name: str = 'John Doe'  
+    signup_ts: datetime | None  
+    tastes: dict[str, PositiveInt]
     username: str
     email: EmailStr
