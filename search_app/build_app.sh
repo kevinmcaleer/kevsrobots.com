f9b0ef03003ea375e47880259d02ba22e40a50e3
Kevin McAleer <kevinmcaleer@gmail.com>
2024-02-03 12:29:49 +0000

    updated build_app

--- search_app/build_app.sh
+++ search_app/build_app.sh
@@ -1,6 +1,6 @@
 # Build your Docker image
-docker build -t search:latest .
+docker build -t http://192.168.2.1:5000/search:latest .
 
 # Push your Docker image
 # docker push search:latest
-docker push 192.168.2.1:5000/search:latest
+docker push http://192.168.2.1:5000/search:latest
